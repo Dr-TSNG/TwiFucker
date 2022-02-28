6f94bac062febb4a9d6cc28e2f03e8749012646b
Nullptr <52071314+Dr-TSNG@users.noreply.github.com>
2022-02-28 16:19:19 +0800

    Update gradle

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "icu.nullptr.twifucker"
         minSdk = 24
         targetSdk = 32
-        versionCode = 2
-        versionName = "1.0.1"
+        versionCode = 3
+        versionName = "1.1"
     }
 
     signingConfigs.create("config") {
--- build.gradle.kts
+++ build.gradle.kts
@@ -5,7 +5,7 @@ buildscript {
         maven("https://jitpack.io")
     }
     dependencies {
-        classpath("com.android.tools.build:gradle:7.1.1")
+        classpath("com.android.tools.build:gradle:7.1.2")
     }
 }
 

09721776b6279f72851bffd07c4b4e774317fb82
Jason_Khew <29531167+JasonKhew96@users.noreply.github.com>
2022-03-16 14:36:33 +0800

    refactor: simplify json hook

--- app/src/main/java/icu/nullptr/twifucker/JsonHook.kt
+++ app/src/main/java/icu/nullptr/twifucker/JsonHook.kt
@@ -8,32 +8,105 @@ import org.json.JSONObject
 import java.io.BufferedReader
 import java.io.InputStream
 
-fun removeTimelineAds(entries: JSONArray) {
-    val removeIndex = mutableListOf<Int>()
-    entries.forEachIndexed<JSONObject> { index, entry ->
-        if (entry.optString("entryId").startsWith("promotedTweet-")) {
-            removeIndex.add(index)
-            Log.d("Handle timeline ads")
-        }
-    }
-    for (i in removeIndex.reversed()) {
-        entries.remove(i)
+// root
+fun JSONObject.jsonGetTweets(): JSONObject? =
+    optJSONObject("globalObjects")?.optJSONObject("tweets")
+
+fun JSONObject.jsonGetInstructions(): JSONArray? =
+    optJSONObject("timeline")?.optJSONArray("instructions")
+
+fun JSONObject.jsonGetData(): JSONObject? = optJSONObject("data")
+
+// data
+fun JSONObject.dataGetInstructions(): JSONArray? =
+    optJSONObject("user_result")?.optJSONObject("result")?.optJSONObject("timeline_response")
+        ?.optJSONObject("timeline")?.optJSONArray("instructions") ?: optJSONObject(
+        "timeline_response"
+    )?.optJSONArray("instructions")
+
+// tweets
+fun JSONObject.tweetsForEach(action: (JSONObject) -> Unit) {
+    for (i in keys()) {
+        optJSONObject(i)?.let { action(it) }
     }
 }
 
-fun removePromotedWhoToFollow(entries: JSONArray) {
-    entries.forEach<JSONObject> { entry ->
-        if (entry.optString("entryId").startsWith("whoToFollow-")) {
-            val items = entry.optJSONObject("content")?.optJSONArray("items")
+// tweet
+fun JSONObject.tweetGetExtendedEntitiesMedias(): JSONArray? =
+    optJSONObject("extended_entities")?.optJSONArray("media")
+
+// entry
+fun JSONObject.entryHasPromotedMetadata(): Boolean =
+    optJSONObject("content")?.optJSONObject("item")?.optJSONObject("content")
+        ?.optJSONObject("tweet")?.has("promotedMetadata") == true
+
+fun JSONObject.entryHasWhoToFollow(): Boolean = optString("entryId").startsWith("whoToFollow-")
+
+fun JSONObject.entryGetWhoToFollowItems(): JSONArray? =
+    optJSONObject("content")?.optJSONArray("items")
+        ?: optJSONObject("content")?.optJSONObject("timelineModule")?.optJSONArray("items")
+
+fun JSONObject.entryIsTweet(): Boolean = optString("entryId").startsWith("tweet-")
+
+fun JSONObject.entryGetLegacy(): JSONObject? =
+    optJSONObject("content")?.optJSONObject("content")?.optJSONObject("tweetResult")
+        ?.optJSONObject("result")?.optJSONObject("legacy")
+
+// legacy
+fun JSONObject.legacyGetRetweetedStatusLegacy(): JSONObject? =
+    optJSONObject("retweeted_status_result")?.optJSONObject("result")?.optJSONObject("legacy")
+
+fun JSONObject.legacyGetExtendedEntitiesMedias(): JSONArray? =
+    optJSONObject("extended_entities")?.optJSONArray("media")
+
+// item
+fun JSONObject.itemContainsPromotedUser(): Boolean =
+    optJSONObject("item")?.optJSONObject("content")?.has("userPromotedMetadata") == true
+
+// timeline
+fun JSONObject.timelinePinEntry(): JSONObject? = optJSONObject("entry")
+fun JSONObject.timelineAddEntries(): JSONArray? = optJSONArray("entries")
+
+// instruction
+fun JSONObject.instructionIsTimelineAddEntries(): Boolean =
+    optString("__typename") == "TimelineAddEntries"
+
+fun JSONObject.instructionIsTimelinePinEntry(): Boolean =
+    optString("__typename") == "TimelinePinEntry"
+
+fun JSONObject.instructionGetAddEntries(): JSONArray? =
+    optJSONObject("addEntries")?.optJSONArray("entries")
+
+// media
+fun JSONObject.mediaHasSensitiveMediaWarning(): Boolean = has("sensitive_media_warning")
+fun JSONObject.mediaRemoveSensitiveMediaWarning(): Any? = remove("sensitive_media_warning")
+
+// entries
+fun JSONArray.entriesRemoveTimelineAds() {
+    val removeIndex = mutableListOf<Int>()
+    forEachIndexed<JSONObject> { index, entry ->
+        if (entry.entryHasPromotedMetadata()) {
+            removeIndex.add(index)
+            Log.d("Handle timeline ads $index")
+        }
+    }
+    for (i in removeIndex.reversed()) {
+        remove(i)
+    }
+}
+
+fun JSONArray.entriesRemovePromotedWhoToFollow() {
+    forEach<JSONObject> { entry ->
+        if (entry.entryHasWhoToFollow()) {
+            val items = entry.entryGetWhoToFollowItems()
             val removeIndex = mutableListOf<Int>()
             items?.forEachIndexed<JSONObject> { index, item ->
-                item.optJSONObject("item")?.optJSONObject("content")?.has("userPromotedMetadata")
-                    ?.let {
-                        if (it) {
-                            removeIndex.add(index)
-                            Log.d("Handle whoToFollow promoted user")
-                        }
+                item.itemContainsPromotedUser().let {
+                    if (it) {
+                        removeIndex.add(index)
+                        Log.d("Handle whoToFollow promoted user $index")
                     }
+                }
             }
             for (i in removeIndex.reversed()) {
                 items?.remove(i)
@@ -42,23 +115,40 @@ fun removePromotedWhoToFollow(entries: JSONArray) {
     }
 }
 
-fun removeSensitiveMediaWarning(entries: JSONArray) {
-    entries.forEach<JSONObject> { entry ->
-        if (entry.optString("entryId").startsWith("tweet-")) {
-            entry.optJSONObject("content")?.optJSONObject("content")?.optJSONObject("tweetResult")
-                ?.optJSONObject("result")?.optJSONObject("legacy")
-                ?.optJSONObject("extended_entities")?.optJSONArray("media")
+fun JSONObject.entryRemoveSensitiveMediaWarning() {
+    if (entryIsTweet()) {
+        entryGetLegacy()?.let {
+            it.legacyGetExtendedEntitiesMedias()?.forEach<JSONObject> { media ->
+                if (media.mediaHasSensitiveMediaWarning()) {
+                    media.mediaRemoveSensitiveMediaWarning()
+                    Log.d("Handle sensitive media warning")
+                }
+            }
+            it.legacyGetRetweetedStatusLegacy()?.legacyGetExtendedEntitiesMedias()
                 ?.forEach<JSONObject> { media ->
-                    if (media.has("sensitive_media_warning")) {
-                        media.remove("sensitive_media_warning")
-                        Log.d("Handle tweet sensitive media warning")
+                    if (media.mediaHasSensitiveMediaWarning()) {
+                        media.mediaRemoveSensitiveMediaWarning()
+                        Log.d("Handle sensitive media warning")
                     }
                 }
         }
     }
 }
 
-fun doHandleJson(param: XC_MethodHook.MethodHookParam) {
+fun JSONArray.entriesRemoveSensitiveMediaWarning() {
+    forEach<JSONObject> { entry ->
+        entry.entryRemoveSensitiveMediaWarning()
+    }
+}
+
+fun JSONArray.entriesRemoveAnnoyance() {
+    entriesRemoveTimelineAds()
+    entriesRemovePromotedWhoToFollow()
+    entriesRemoveSensitiveMediaWarning()
+}
+
+
+fun handleJson(param: XC_MethodHook.MethodHookParam) {
     val inputStream = param.result as InputStream
     val reader = BufferedReader(inputStream.reader())
     var content: String
@@ -69,48 +159,37 @@ fun doHandleJson(param: XC_MethodHook.MethodHookParam) {
     try {
         val json = JSONObject(content)
 
-        // home timeline
-        json.optJSONObject("globalObjects")?.optJSONObject("tweets")?.let { tweets ->
-            tweets.keys().forEach { tweet ->
-                tweets.optJSONObject(tweet)?.optJSONObject("extended_entities")
-                    ?.optJSONArray("media")?.forEach<JSONObject> { media ->
-                        if (media.has("sensitive_media_warning")) {
-                            media.remove("sensitive_media_warning")
-                            Log.d("Handle timeline sensitive media warning")
-                        }
-                    }
-            }
-        }
-        json.optJSONObject("timeline")?.optJSONArray("instructions")
-            ?.forEach<JSONObject> { instruction ->
-                instruction.optJSONObject("addEntries")?.optJSONArray("entries")?.let {
-                    removeTimelineAds(it)
+        json.jsonGetTweets()?.tweetsForEach { tweet ->
+            tweet.tweetGetExtendedEntitiesMedias()?.forEach<JSONObject> { media ->
+                if (media.mediaHasSensitiveMediaWarning()) {
+                    media.mediaRemoveSensitiveMediaWarning()
+                    Log.d("Handle sensitive media warning")
                 }
             }
+        }
+        json.jsonGetInstructions()?.forEach<JSONObject> { instruction ->
+            if (instruction.instructionIsTimelineAddEntries()) {
+                instruction.timelineAddEntries()?.entriesRemoveAnnoyance()
+            }
+            if (instruction.instructionIsTimelinePinEntry()) {
+                instruction.timelinePinEntry()?.entryRemoveSensitiveMediaWarning()
+            }
+            instruction.instructionGetAddEntries()?.entriesRemoveAnnoyance()
+        }
+        json.jsonGetData()?.dataGetInstructions()?.forEach<JSONObject> { instruction ->
+            if (instruction.instructionIsTimelineAddEntries()) {
+                instruction.timelineAddEntries()?.entriesRemoveAnnoyance()
+            }
+            if (instruction.instructionIsTimelinePinEntry()) {
+                instruction.timelinePinEntry()?.entryRemoveSensitiveMediaWarning()
+            }
+            instruction.instructionGetAddEntries()?.entriesRemoveAnnoyance()
+        }
 
-        json.optJSONObject("data")?.let { data ->
-            // user profile timeline query
-            data.optJSONObject("user_result")?.optJSONObject("result")
-                ?.optJSONObject("timeline_response")?.optJSONObject("timeline")
-                ?.optJSONArray("instructions")?.forEach<JSONObject> { instruction ->
-                    if (instruction.optString("__typename") == "TimelineAddEntries") {
-                        instruction.optJSONArray("entries")?.let {
-                            removeSensitiveMediaWarning(it)
-                            removePromotedWhoToFollow(it)
-                            removeTimelineAds(it)
-                        }
-                    }
-                }
-            // tweet details
-            data.optJSONObject("timeline_response")?.optJSONArray("instructions")
-                ?.forEach<JSONObject> { instruction ->
-                    instruction.optJSONArray("entries")?.let {
-                        removeSensitiveMediaWarning(it)
-                    }
-                }
-        }
         content = json.toString()
     } catch (_: JSONException) {
+    } catch (e: Throwable) {
+        Log.e(e)
     }
     param.result = content.byteInputStream()
 }
@@ -118,7 +197,7 @@ fun doHandleJson(param: XC_MethodHook.MethodHookParam) {
 fun jsonHook() {
     try {
         findMethod("com.fasterxml.jackson.core.b") { name == "i" }.hookAfter { param ->
-            doHandleJson(param)
+            handleJson(param)
         }
     } catch (e: Throwable) {
         Log.e(e)

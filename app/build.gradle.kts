a2a65fe275133ee34cc87b59fe8c42bde7fe5caf
Js0n <29531167+JasonKhew96@users.noreply.github.com>
2022-08-19 18:20:27 +0800

    fix: resources

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -80,7 +80,6 @@ android {
 
         release {
             isMinifyEnabled = true
-            isShrinkResources = true
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.pro"

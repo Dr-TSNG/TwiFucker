61cf9a12047604512a1afae5a00877580135a84a
Js0n <29531167+JasonKhew96@users.noreply.github.com>
2022-08-22 14:08:57 +0800

    feat: delete promoted tweets from database (#48)

--- app/src/main/java/icu/nullptr/twifucker/ui/SettingsDialog.kt
+++ app/src/main/java/icu/nullptr/twifucker/ui/SettingsDialog.kt
@@ -7,13 +7,16 @@ import android.app.AlertDialog
 import android.content.Context
 import android.content.Intent
 import android.content.SharedPreferences
+import android.database.sqlite.SQLiteDatabase
 import android.net.Uri
 import android.os.Bundle
 import android.preference.Preference
 import android.preference.PreferenceFragment
+import com.github.kyuubiran.ezxhelper.utils.Log
 import com.github.kyuubiran.ezxhelper.utils.addModuleAssetPath
 import com.github.kyuubiran.ezxhelper.utils.restartHostApp
 import icu.nullptr.twifucker.R
+import icu.nullptr.twifucker.hook.modulePrefs
 
 class SettingsDialog(context: Context) : AlertDialog.Builder(context) {
 
@@ -21,6 +24,25 @@ class SettingsDialog(context: Context) : AlertDialog.Builder(context) {
         private lateinit var outDialog: AlertDialog
         private lateinit var prefs: SharedPreferences
         const val PREFS_NAME = "twifucker"
+    }
+
+    private fun deleteFromDatabase() {
+        val disablePromotedContent = modulePrefs.getBoolean("disable_promoted_content", true)
+        if (!disablePromotedContent) return
+        val re = Regex("^\\d+-\\d+\\.db$")
+        var count = 0
+        context.databaseList().forEach { db ->
+            if (re.matches(db)) {
+                val database = SQLiteDatabase.openDatabase(
+                    context.getDatabasePath(db).absolutePath, null, SQLiteDatabase.OPEN_READWRITE
+                )
+                count += database.delete(
+                    "timeline", "entity_id LIKE ?", arrayOf("promotedTweet-%")
+                )
+                database.close()
+            }
+        }
+        Log.toast(context.getString(R.string.deleted_n_promoted_tweet, count))
     }
 
     class PrefsFragment : PreferenceFragment(), Preference.OnPreferenceChangeListener,
@@ -62,6 +84,7 @@ class SettingsDialog(context: Context) : AlertDialog.Builder(context) {
 
             setTitle(context.getString(R.string.twifucker_settings))
             setPositiveButton(context.getString(R.string.save_restart)) { _, _ ->
+                deleteFromDatabase()
                 restartHostApp(act)
             }
             setNegativeButton(context.getString(R.string.settings_dismiss), null)

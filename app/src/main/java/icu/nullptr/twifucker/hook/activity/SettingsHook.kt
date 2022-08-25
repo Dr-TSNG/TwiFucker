dbda6fdcfe0cb89effe5cc1f0d87b6f2f982fef5
Js0n <29531167+JasonKhew96@users.noreply.github.com>
2022-08-25 14:00:49 +0800

    fix: settings hook for version 9.56

--- app/src/main/java/icu/nullptr/twifucker/hook/activity/SettingsHook.kt
+++ app/src/main/java/icu/nullptr/twifucker/hook/activity/SettingsHook.kt
@@ -1,25 +1,53 @@
 package icu.nullptr.twifucker.hook.activity
 
 import android.app.Activity
-import com.github.kyuubiran.ezxhelper.utils.Log
-import com.github.kyuubiran.ezxhelper.utils.findMethod
 import com.github.kyuubiran.ezxhelper.utils.hookReplace
 import com.github.kyuubiran.ezxhelper.utils.loadClass
 import icu.nullptr.twifucker.hook.BaseHook
+import icu.nullptr.twifucker.hook.HookEntry.Companion.dexHelper
+import icu.nullptr.twifucker.hook.HookEntry.Companion.loadDexHelper
 import icu.nullptr.twifucker.ui.SettingsDialog
 
 object SettingsHook : BaseHook() {
     override fun init() {
-        try {
-            findMethod("com.twitter.app.settings.AboutActivity") {
-                parameterTypes.size == 1 && parameterTypes[0] == loadClass("android.preference.Preference")
-            }.hookReplace { param ->
+        val aboutActivityClass = loadClass("com.twitter.app.settings.AboutActivity")
+        val preferenceClass = loadClass("android.preference.Preference")
+        val onVersionClickMethod = aboutActivityClass.declaredMethods.firstOrNull {
+            it.parameterTypes.size == 1 && it.parameterTypes[0] == preferenceClass
+        }
+        if (onVersionClickMethod != null) {
+            onVersionClickMethod.hookReplace { param ->
                 SettingsDialog(param.thisObject as Activity)
                 return@hookReplace true
             }
-        } catch (t: Throwable) {
-            Log.e(t)
+        } else {
+            loadDexHelper()
+            val onCreateMethod = aboutActivityClass.declaredMethods.firstOrNull {
+                it.name == "onCreate"
+            } ?: throw NoSuchMethodError()
+            val initMethodIndex = dexHelper.findMethodInvoking(
+                dexHelper.encodeMethodIndex(onCreateMethod),
+                dexHelper.encodeClassIndex(Void.TYPE),
+                1,
+                null,
+                -1,
+                longArrayOf(dexHelper.encodeClassIndex(aboutActivityClass)),
+                null,
+                null,
+                true,
+            ).first()
+            val onPreferenceClickListenerClass =
+                dexHelper.decodeMethodIndex(initMethodIndex)?.declaringClass
+                    ?: throw ClassNotFoundException()
+            val activityField = onPreferenceClickListenerClass.declaredFields.firstOrNull {
+                it.type == aboutActivityClass
+            } ?: throw NoSuchFieldError()
+            onPreferenceClickListenerClass.declaredMethods.first {
+                it.parameterTypes.size == 1 && it.parameterTypes[0] == preferenceClass
+            }.hookReplace { param ->
+                SettingsDialog(activityField.get(param.thisObject) as Activity)
+                return@hookReplace true
+            }
         }
     }
-
 }

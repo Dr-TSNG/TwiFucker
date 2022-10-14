d531fac32fc3fedcc5546fe6a7e4e63023b5639d
Js0n <29531167+JasonKhew96@users.noreply.github.com>
2022-10-14 14:37:13 +0800

    chore: upgrade AGP

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,7 +5,7 @@ buildscript {
         maven("https://jitpack.io")
     }
     dependencies {
-        classpath("com.android.tools.build:gradle:7.3.0")
+        classpath("com.android.tools.build:gradle:7.3.1")
     }
 }
 

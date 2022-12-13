3bb9cbcc9ccf19d5168e4391e7b7365c6686c8a1
Js0n <29531167+JasonKhew96@users.noreply.github.com>
2022-12-13 12:58:18 +0800

    [skip ci] fix: json timeline module item hook null check and less logging

--- app/src/main/java/icu/nullptr/twifucker/hook/JsonTimelineModuleItemHook.kt
+++ app/src/main/java/icu/nullptr/twifucker/hook/JsonTimelineModuleItemHook.kt
@@ -5,9 +5,12 @@ import com.github.kyuubiran.ezxhelper.utils.findMethod
 import com.github.kyuubiran.ezxhelper.utils.hookAfter
 import com.github.kyuubiran.ezxhelper.utils.loadClass
 import icu.nullptr.twifucker.isEntryNeedsRemove
+import icu.nullptr.twifucker.modulePrefs
 
 object JsonTimelineModuleItemHook : BaseHook() {
     override fun init() {
+        if (!modulePrefs.getBoolean("disable_who_to_follow", false)) return
+
         val jsonTimelineModuleItemClass =
             loadClass("com.twitter.model.json.timeline.urt.JsonTimelineModuleItem")
 
@@ -37,6 +40,7 @@ object JsonTimelineModuleItemHook : BaseHook() {
         findMethod(jsonAddToModuleInstructionMapperClass) {
             name == "_parse" && returnType == jsonAddToModuleInstructionClass
         }.hookAfter { param ->
+            if (param.result == null) return@hookAfter
             jsonTimelineModuleItemField1.get(param.result).let { itemsRaw ->
                 val items = itemsRaw as ArrayList<*>
                 val itemsNeedRemove = mutableListOf<Any>()
@@ -45,17 +49,20 @@ object JsonTimelineModuleItemHook : BaseHook() {
                         val entryId = entryIdRaw as String
                         if (isEntryNeedsRemove(entryId)) {
                             itemsNeedRemove.add(item)
-                            Log.d("Remove timeline module item $entryId")
                         }
                     }
                 }
                 items.removeAll(itemsNeedRemove.toSet())
+                if (items.removeAll(itemsNeedRemove.toSet())) {
+                    Log.d("Remove ${itemsNeedRemove.size} timeline module item(s)")
+                }
             }
         }
 
         findMethod(jsonTimelineModuleMapperClass) {
             name == "_parse" && returnType == jsonTimelineModuleClass
         }.hookAfter { param ->
+            if (param.result == null) return@hookAfter
             jsonTimelineModuleItemField2.get(param.result).let { itemsRaw ->
                 val items = itemsRaw as ArrayList<*>
                 val itemsNeedRemove = mutableListOf<Any>()
@@ -64,11 +71,12 @@ object JsonTimelineModuleItemHook : BaseHook() {
                         val entryId = entryIdRaw as String
                         if (isEntryNeedsRemove(entryId)) {
                             itemsNeedRemove.add(item)
-                            Log.d("Remove timeline module item $entryId")
                         }
                     }
                 }
-                items.removeAll(itemsNeedRemove.toSet())
+                if (items.removeAll(itemsNeedRemove.toSet())) {
+                    Log.d("Remove ${itemsNeedRemove.size} timeline module item(s)")
+                }
             }
         }
     }

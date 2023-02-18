831cf0553af0ab7e47624f55955e9028a53361b3
Js0n <29531167+JasonKhew96@users.noreply.github.com>
2023-02-19 00:41:28 +0800

    refactor: redesign key value view

---
+++ app/src/main/java/icu/nullptr/twifucker/ui/KeyValueView.kt
@@ -0,0 +1,53 @@
+package icu.nullptr.twifucker.ui
+
+import android.content.Context
+import android.view.ViewGroup.LayoutParams.MATCH_PARENT
+import android.view.ViewGroup.LayoutParams.WRAP_CONTENT
+import android.widget.EditText
+import android.widget.Switch
+import icu.nullptr.twifucker.R
+
+class KeyValueView(context: Context) : CustomLayout(context) {
+
+    val editText = EditText(context).apply {
+        setTextAppearance(android.R.style.TextAppearance_DeviceDefault_Medium)
+        layoutParams = LayoutParams(MATCH_PARENT, WRAP_CONTENT).apply {
+            marginStart = 32.dp
+            marginEnd = 32.dp
+        }
+        hint = context.getString(R.string.feature_switch_key_hint)
+        addView(this)
+    }
+
+    val switch = Switch(context).apply {
+        setTextAppearance(android.R.style.TextAppearance_DeviceDefault_Medium)
+        layoutParams = LayoutParams(MATCH_PARENT, WRAP_CONTENT).apply {
+            marginStart = 32.dp
+            marginEnd = 32.dp
+            setPadding(8.dp, 0, 0, 0)
+        }
+        text = context.getString(R.string.feature_switch_value_boolean)
+        addView(this)
+    }
+
+    override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec)
+        val maxWidth = measuredWidth - editText.marginLeft - editText.marginRight
+        editText.measure(
+            maxWidth.toExactlyMeasureSpec(), editText.defaultHeightMeasureSpec(this)
+        )
+        switch.measure(
+            (maxWidth - switch.paddingLeft).toExactlyMeasureSpec(),
+            switch.defaultHeightMeasureSpec(this)
+        )
+        setMeasuredDimension(
+            maxWidth.coerceAtLeast(measuredWidth),
+            editText.measuredHeightWithMargins + switch.measuredHeightWithMargins
+        )
+    }
+
+    override fun onLayout(changed: Boolean, l: Int, t: Int, r: Int, b: Int) {
+        editText.let { it.layout(x = it.marginLeft, y = 0) }
+        switch.let { it.layout(x = it.marginLeft, y = editText.measuredHeightWithMargins) }
+    }
+}

f58a5ae91f6b279e6b1976f2d2ea67dc07ecd574
Nullptr <noreply@nullptr.icu>
2023-03-04 21:39:38 +0800

    Static library

--- app/src/main/cpp/external/Android.mk
+++ app/src/main/cpp/external/Android.mk
@@ -3,4 +3,4 @@ LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 LOCAL_MODULE            := linux-syscall-support
 LOCAL_EXPORT_C_INCLUDES := $(LOCAL_PATH)/linux-syscall-support
-include $(BUILD_SHARED_LIBRARY)
+include $(BUILD_STATIC_LIBRARY)
